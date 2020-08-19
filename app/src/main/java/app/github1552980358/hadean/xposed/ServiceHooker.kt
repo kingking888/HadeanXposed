c22073aaa0b344a36d545e559928b93d477e7ad2
1552980358 <1552980358@qq.com>
2020-08-19 17:59:13 +0800

    ServiceHooker.kt: Inherit and move methods into BaseHooker
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/app/github1552980358/hadean/xposed/ServiceHooker.kt
+++ app/src/main/java/app/github1552980358/hadean/xposed/ServiceHooker.kt
@@ -1,6 +1,8 @@
 package app.github1552980358.hadean.xposed
 
 import android.app.Service
+import android.content.Intent
+import app.github1552980358.hadean.xposed.base.BaseHooker
 import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -13,57 +15,108 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
  * @time    : 20:54
  **/
 
-interface ServiceHooker {
+interface ServiceHooker: BaseHooker {
     
     fun hookService(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
-        hookConstructor(loadPackageParam)
+        // hookConstructor(loadPackageParam)
+        hookConstructor(
+            "android.app.Service",
+            loadPackageParam,
+            mutableListOf()
+        ) {
+            // Just throw Exception to clean stack
+            throw Exception()
+        }
         hookOnCreate(loadPackageParam)
         hookOnStartCommand(loadPackageParam)
     }
     
+    /**
+     * @hide
+     **/
+    @Deprecated("See [hookConstructor] of [BaseHooker]")
     private fun hookConstructor(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookConstructor("android.app.Service", loadPackageParam.classLoader, object: XC_MethodReplacement() {
-            override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                // Just throw Exception to clean stack
-                throw Exception()
-            }
-        })
+        XposedHelpers.findAndHookConstructor(
+            "android.app.Service",
+            loadPackageParam.classLoader,
+            object: XC_MethodReplacement() {
+                override fun replaceHookedMethod(param: MethodHookParam?): Any? {
+                    // Just throw Exception to clean stack
+                    throw Exception()
+                }
+            })
     }
     
     private fun hookOnCreate(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookMethod(
+        replaceMethod(
             "android.app.Service",
-            loadPackageParam.classLoader,
+            loadPackageParam,
             "onCreate",
-            object: XC_MethodReplacement() {
-                override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                    // Just throw Exception to clean stack
-                    throw Exception()
-                }
-            })
+            mutableListOf()
+        ) {
+            // Just throw Exception to clean stack
+            throw Exception()
+        }
+        /**
+         * XposedHelpers.findAndHookMethod(
+         *     "android.app.Service",
+         *     loadPackageParam.classLoader,
+         *     "onCreate",
+         *     object: XC_MethodReplacement() {
+         *         override fun replaceHookedMethod(param: MethodHookParam?): Any? {
+         *             // Just throw Exception to clean stack
+         *             throw Exception()
+         *         }
+         *    })
+         **/
     }
     
     private fun hookOnStartCommand(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookMethod(
+        replaceMethod(
             "android.app.Service",
-            loadPackageParam.classLoader, "onStartCommand",
-            object: XC_MethodReplacement() {
-                override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                    
-                    // Stop foreground
-                    try {
-                        (param?.thisObject as Service?)?.apply {
-                            stopForeground(true)
-                            stopSelf()
-                        }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                    
-                    // Just throw Exception to clean stack
-                    throw Exception()
+            loadPackageParam,
+            "onStartCommand",
+            mutableListOf(
+                Intent::class.java,
+                Int::class.java,
+                Int::class.java
+            )
+        ) { param ->
+            // Stop foreground
+            try {
+                (param?.thisObject as Service?)?.apply {
+                    stopForeground(true)
+                    stopSelf()
                 }
-            })
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            
+            // Just throw Exception to clean stack
+            throw Exception()
+        }
+        /**
+         * XposedHelpers.findAndHookMethod(
+         *     "android.app.Service",
+         *     loadPackageParam.classLoader, "onStartCommand",
+         *     object: XC_MethodReplacement() {
+         *         override fun replaceHookedMethod(param: MethodHookParam?): Any? {
+         *
+         *             // Stop foreground
+         *             try {
+         *                 (param?.thisObject as Service?)?.apply {
+         *                     stopForeground(true)
+         *                     stopSelf()
+         *                 }
+         *             } catch (e: Exception) {
+         *                 e.printStackTrace()
+         *             }
+         *
+         *             // Just throw Exception to clean stack
+         *             throw Exception()
+         *         }
+         *     })
+         **/
     }
     
 }
